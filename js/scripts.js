89f45d82fb4dceff5b3c18509381967e1349c403
Isabel Hasse <isabelhasse2@gmail.com>
2017-12-12 17:28:10 -0800

    add start page with placeholders for pet image and description, add js to generate random number and choose between 3 pets.

--- js/scripts.js
+++ js/scripts.js
@@ -13,42 +13,59 @@ function meter(stat) {
 };
 
 $(document).ready(function() {
-  var petsName = prompt("What do you want to name your pet?")
-  var pet = new Pet(petsName);
-  $("#pets-name").text(pet.name);
+  var random =  Math.floor(Math.random() * 3) + 1;
+  if (random = 1) {
+    var petImg = "(img url)";
+    var petDescription = "";
+  } else if (random = 2) {
+    var petImg = "(img url)";
+    var petDescription = "";
+  } else {
+    var petImg = "(img url)";
+    var petDescription = "";
+  }
 
-  var timer = setInterval(decrease, 1000);
-  $("#hunger").text(meter(pet.hunger));
-  $("#happiness").text(meter(pet.happiness));
+  $("#name-form").submit(function(event) {
+    event.preventDefault();
+    var petsName = $("input#name").val();
+    var pet = new Pet(petsName);
+    $("#pets-name").text(pet.name);
 
-  function decrease() {
-    if (pet.hunger != 0) {
-      pet.hunger -= 1;
-      $("#hunger").text(meter(pet.hunger));
-    }
-    if (pet.happiness != 0) {
-      pet.happiness -= 1;
-      $("#happiness").text(meter(pet.happiness));
-    }
-  };
+    $("#start").hide();
+    $("#main-screen").show();
 
-  $("#feed").click(function() {
-    if (pet.hunger <= 9) {
-      pet.hunger += 3;
-    } else if (pet.hunger <= 12){
-      pet.hunger = 12;
-    }
+    var timer = setInterval(decrease, 1000);
     $("#hunger").text(meter(pet.hunger));
-  });
-
-  $("#happy").click(function() {
-    if (pet.happiness <= 9) {
-      pet.happiness += 3;
-    } else if (pet.happiness <= 12) {
-      pet.happiness = 12;
-    }
     $("#happiness").text(meter(pet.happiness));
-  });
 
+    function decrease() {
+      if (pet.hunger != 0) {
+        pet.hunger -= 1;
+        $("#hunger").text(meter(pet.hunger));
+      }
+      if (pet.happiness != 0) {
+        pet.happiness -= 1;
+        $("#happiness").text(meter(pet.happiness));
+      }
+    };
+
+    $("#feed").click(function() {
+      if (pet.hunger <= 9) {
+        pet.hunger += 3;
+      } else if (pet.hunger <= 12){
+        pet.hunger = 12;
+      }
+      $("#hunger").text(meter(pet.hunger));
+    });
+
+    $("#happy").click(function() {
+      if (pet.happiness <= 9) {
+        pet.happiness += 3;
+      } else if (pet.happiness <= 12) {
+        pet.happiness = 12;
+      }
+      $("#happiness").text(meter(pet.happiness));
+    });
+  })
 
 });
